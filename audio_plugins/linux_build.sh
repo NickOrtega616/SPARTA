252fcc6d0209692a57ed9d9317c75f98db774313
Marc Lavallée <marc@hacklava.net>
2019-11-20 19:36:57 -0500

    fix linux build script

--- audio_plugins/linux_build.sh
+++ audio_plugins/linux_build.sh
@@ -1,71 +1,90 @@
 #!/bin/bash
 
-# script to "projuce" and build JUCER plugins on Linux
+help_message="
+script to \"projuce\" and build JUCER plugins on Linux
 
-# parameters:
-# without parameters the script will show info about the plugin(s)
-# - a directory: the script use it as a starting point (only one)
-# - "build": to build the plugin(s)
-# "projucer": to open Projucer for plugins(s)
-# "projuce": to regenerate Projucer files
-# remaining params are passed to "make"
-# ex: "_SPARTA_ambiBIN_ Projuce" open Projucer for _SPARTA_ambiBIN_ plugin
-# ex: "projuce build clean" generates all Projucer files and cleans all builds
-# ex: "_SPARTA_ambiBIN_ build -j6" builds _SPARTA_ambiBIN_ using 6 cores
+parameters:
+without parameters the script will show info about the plugin(s)
+- \"help\": to help this message
+- a directory: the script use it as a starting point (only one)
+- \"projucer\": to open Projucer for plugins(s)
+- \"projuce\": to regenerate Projucer files
+- \"clean\": to clean the plugin(s)
+- \"build\": to build the plugin(s)
+remaining params are passed to \"make\"
+
+ex: \"_SPARTA_ambiBIN_ projucer\" opens Projucer for the \"_SPARTA_ambiBIN_\" plugin
+ex: \"_SPARTA_ambiBIN_ build -j6\" builds _SPARTA_ambiBIN_ using 6 cores
+ex: \"projuce clean build\" generates all Projucer files, clean and build all plugins
+"
 
 from="."
+help=0
+info=0
 build=0
+clean=0
 projucer=0
 projuce=0
 
 # location of plugin binaries
 binaries="../lib"
 
-i=1
-while [ $i -le $# ]
-  do
-    var="$1"
-    if [ -d ${var} ]; then
-      from=${var}
-      i=$(($i-1))
-    elif [ ${var} == "build" ]; then
-      build=1
-      i=$(($i-1))
-    elif [ ${var} == "projucer" ]; then
-      projucer=1
-      i=$(($i-1))
-    elif [ ${var} == "projuce" ]; then
-      projuce=1
-      i=$(($i-1))
-    else
-      set -- "$@" "$var"
-    fi
-    shift
-    i=$(($i+1))
+i=$#
+while [ $i -gt 0 ]; do
+  var=$1
+  if [ -d ${var} ]; then
+    from=${var}
+  elif [ ${var} == "help" ]; then
+    help=1
+  elif [ ${var} == "info" ]; then
+    info=1
+  elif [ ${var} == "projucer" ]; then
+    projucer=1
+  elif [ ${var} == "projuce" ]; then
+    projuce=1
+  elif [ ${var} == "clean" ]; then
+    clean=1
+  elif [ ${var} == "build" ]; then
+    build=1
+  else
+    set -- "$@" "$var"
+  fi
+  i=$(($i-1))
+  shift
 done
 
-let "info = ${build} + ${projucer} + ${projuce}"
-if [ ${info} -eq 0 ]; then
-    if [ ${from} = "." ]; then
-      ls -Rl --color ${binaries}
-    else
-      jucer=$(find "${from}" -type f -name "*.jucer")
-      status="$(Projucer --status ${jucer} 2>&1)"
-      echo "${status}"
-      name=$(echo "${status}" | grep Name | cut -c 7-)
-      ls -Rl --color ${binaries}/${name}
-    fi
-exit
+if [ $(( info + build + projucer + projuce )) -eq 0 ]; then help=1; fi
+if [ ${help} -gt 0 ]; then
+  echo "${help_message}"
+  exit
+fi
+
+jucer=$(find "${from}" -type f -name "*.jucer")
+status="$(Projucer --status ${jucer} 2>&1)"
+name=$(echo "${status}" | grep Name | cut -c 7-)
+
+if [ ${info} -gt 0 ]; then
+  if [ ${from} = "." ]; then
+    ls -Rl --color ${binaries}
+  else
+    echo "${status}"
+    ls -Rl --color ${binaries}/${name}
+  fi
+  exit
 fi
 
 # opening Projucer editor
 [ ${projucer} -gt 0 ] && find ${from} -type f -name "*.jucer" \
-    -exec Projucer "{}" \;
+  -exec Projucer "{}" \;
 
 # projucing (resaving Projucer files)
 [ ${projuce} -gt 0 ] && find ${from} -type f -name "*.jucer" \
-    -exec Projucer --resave "{}" \;
+  -exec Projucer --resave "{}" \;
 
-# building (or cleaning)
+# cleaning)
 [ ${build} -gt 0 ] && find "${from}" -type d -name "LinuxMakefile" \
-    -exec bash -c "cd \"{}\" && make CONFIG=Release $@" \;
+  -exec bash -c "cd \"{}\" && make CONFIG=Release clean" \;
+
+# building
+[ ${build} -gt 0 ] && find "${from}" -type d -name "LinuxMakefile" \
+  -exec bash -c "cd \"{}\" && make CONFIG=Release $@" \;
